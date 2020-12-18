54574939a42e0be2bef22cca880922947c0b6ca8
fortunato <sambataro1990@gmail.com>
2020-12-18 17:33:34 +0100

    milestone 1 completata

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var app = new Vue (
     data: {
       moviesResult: [],
       research: "",
-
+      adress: "https://image.tmdb.org/t/p/w220_and_h330_face",
     },
 
     methods: {
@@ -20,10 +20,7 @@ var app = new Vue (
           .get("https://api.themoviedb.org/3/search/movie?api_key=6ef857fc5320b290e8bcd3f87290f56a&language=it-IT&page=1&include_adult=false&query="+self.research )
           .then( (result) => {
             self.moviesResult = result.data.results;
-            self.moviesResult.adress = "https://image.tmdb.org/t/p/w220_and_h330_face/aKTMkzvU4rbABmbVHKs1iiIInxL.jpg"
-            console.log(self.moviesResult);
-            console.log(self.adress);
-
+            console.log(this.moviesResult);
             return this.research = "";
           });
 
